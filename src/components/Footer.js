d7b0ab03810495414f5eeabf1f73a661487032a2
mrdang <khacquyet.dang@gmail.com>
2017-07-30 12:14:52 +0200

    -- add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './styles/Footer.css';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <div className="FooterContainer">
+          <div className="copyright">App made by @danguito</div>
+      </div>
+    );
+  }
+}
+
+export default Footer;
--- src/components/Root.js
+++ src/components/Root.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import App from './App.js';
+import Footer from './Footer.js';
 import AddRecipeItem from './AddRecipeItem.js';
 
 const Root = ({store }) => (
   <Provider store={store}>
-    <Router basename="/#">
+    <Router>
       <div>
         <Route exact path='/' component={App} />
         <Route path='/add' component={AddRecipeItem}/>
+        <Footer />
       </div>
     </Router>
   </Provider>
